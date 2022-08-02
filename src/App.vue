f10bbb4bbb5db24d1b522ce007de3991f96696e6
brunohs007 <brunoholandasilva@hotmail.com>
2022-08-02 18:52:49 -0300

    ajuste de formulario de cadastro

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
   <div>
     <!-- inicio navbar -->
-    <nav class="nav navbar-expand-sm bg-dark navbar-dark">
-      <div class="container">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <a class="navbar-brand order-md-last">CRUD Vue.js </a>
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <router-link class="nav-link" to="/">Criar novo Perfil</router-link>
           </li>
-        </ul>
-        <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <router-link class="nav-link" to="/listar-perfil">Listar todos os Perfis</router-link>
           </li>
         </ul>
-      </div>
     </nav>
     <!-- fim navbar -->
-    <div class="container">
-      <router-view/>
+    <div class="container mt-5">
+      <router-view></router-view>
     </div>
   </div>
-
 </template>
--- src/components/criar-perfil/CriarPerfilComponents.vue
+++ src/components/criar-perfil/CriarPerfilComponents.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="row justify-content-center">
-    <h1>
-      Criar Perfil :
-    </h1>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h3>Criar Perfil</h3>
+      </div>
+      <div class="card-body">
+        <form @submit.prevent="">
+
+          <div class="form-group">
+            <label class="font-weight-bold">Nome</label>
+            <input type="text" class="form-control" placeholder="Digite seu nome">
+          </div>
+
+          <div class="form-group">
+            <label class="font-weight-bold">Email</label>
+            <input type="email" class="form-control" placeholder="Digite seu email">
+          </div>
+
+          <div class="form-group">
+            <label class="font-weight-bold" for="inputPassword4">Senha</label>
+            <input type="password" class="form-control" id="inputPassword4" placeholder="Senha">
+          </div>
+
+          <div class="form-group">
+            <label class="font-weight-bold">Data de nascimento</label>
+            <input type="date" class="form-control" placeholder="YYYY/MM/DD">
+          </div>
+
+          <div class="form-group">
+            <button type="submit" class=" btn btn-primary">Entrar</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+   name: 'CriarPerfilComponents',
+  },
   data() {
     return {
 
